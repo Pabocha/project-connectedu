8f33dc253110ddbdc064420c790102a01ca2baad
Pablo <Ppabocha03@gmail.com>
2024-05-01 03:27:12 +0000

    make it better

--- connectedu/settings.py
+++ connectedu/settings.py
@@ -154,24 +154,24 @@ WSGI_APPLICATION = 'connectedu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django_tenants.postgresql_backend',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    },
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config()
+#     'default': {
+#         'ENGINE': 'django_tenants.postgresql_backend',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT'),
+#     },
 # }
 
-if "DATABASE_URL" in os.environ:
-    DATABASES['default']=dj_database_url.config(
-        conn_max_age=600, ssl_require=True)
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
+
+# if "DATABASE_URL" in os.environ:
+#     DATABASES['default']=dj_database_url.config(
+#         conn_max_age=600, ssl_require=True)
 
 # BD TESTING
 # DATABASES = {
