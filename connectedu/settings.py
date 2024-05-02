a1526685677ae2a1652ca6191940d2a8588108da
Pablo <Ppabocha03@gmail.com>
2024-05-02 06:06:35 +0000

    configuration database_engine

--- connectedu/settings.py
+++ connectedu/settings.py
@@ -156,24 +156,28 @@ WSGI_APPLICATION = 'connectedu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django_tenants.postgresql_backend',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT'),
+#     },
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django_tenants.postgresql_backend',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    },
+    'default': dj_database_url.config(
+        default='postgres://u47kqkfcs3mtp5:p99deaeeb4bb1c3604c66b1d0acfdb571487704c10a4fe53ea4fd5e734a783fd1@cf9gid2f6uallg.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d6u0skcg1katme?backend=django_tenants.postgresql_backend',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # DATABASES = {
 #     'default': dj_database_url.config(conn_max_age=600)
 # }
 
-# if "DATABASE_URL" in os.environ:
-#     database_url = os.getenv('DATABASE_URL')
-#     print(database_url)
 
 # BD TESTING
 # DATABASES = {
