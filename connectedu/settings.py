1982e75ed7dafeedd614b94f7ebcee0cdab0aa40
Pablo <Ppabocha03@gmail.com>
2024-05-09 23:22:03 +0000

    change conf django_heroku

--- connectedu/settings.py
+++ connectedu/settings.py
@@ -254,4 +254,4 @@ CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 # CELERY_BROKER_URL = 'redis://localhost:6379/0'
 # CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
