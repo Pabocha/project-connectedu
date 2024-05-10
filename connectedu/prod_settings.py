2be84717041ee71f022352f734b06620f59cb471
Pablo <Ppabocha03@gmail.com>
2024-05-10 00:24:20 +0000

    add prod_settings

--- connectedu/prod_settings.py
+++ connectedu/prod_settings.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = ['*']
 
 BASE_DOMAIN = 'connectedut-807b56b6599b.herokuapp'
 
-DATABASES['default'] = dj_database_url.config(engine='django_tenants.postgresql_backend')
+DATABASES['default'] = dj_database_url.config(engine='django_tenants.postgresql_backend', conn_max_age=500)
 
 CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
